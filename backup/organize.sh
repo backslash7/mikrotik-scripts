e35308b31b5ca2e0f9508df7a07b2460b2403191
Lukáš Staňa <lukas.stana@gmail.com>
2012-10-05 09:18:52 +0200

    Added compression of previous backup day to organize.sh

--- backup/organize.sh
+++ backup/organize.sh
@@ -3,9 +3,20 @@
 YEAR=`date +%Y`
 MONTH=`date +%m`
 DAY=`date +%d`
+
+YESTERDAY_YEAR=`date +%Y -d 'yesterday'`
+YESTERDAY_MONTH=`date +%m -d 'yesterday'`
+YESTERDAY_DAY=`date +%d -d 'yesterday'`
+
 DIR="backups/${YEAR}/${MONTH}/${DAY}"
+YESTERDAY_DIR="backups/${YESTERDAY_YEAR}/${YESTERDAY_MONTH}/${YESTERDAY_DAY}"
+
+if [ -d "$YESTERDAY_DIR" ];
+then
+    tar -C $YESTERDAY_DIR -cvzf $YESTERDAY_DIR.tar.gz .
+    rm -rf $YESTERDAY_DIR
+fi
 
 mkdir -p $DIR
 
 mv pending/* $DIR/
-
